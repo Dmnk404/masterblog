d5a5654525379a69e992670fbaf528e36710b823
Dominik Melzner <dominik@Mac.fritz.box>
2025-07-03 16:40:37 +0200

    Added index.html template and style.css. Extended Logic.

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
-
-from flask import Flask
+import json
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
+with open("posts.json", "r", encoding="utf-8") as f:
+    posts = json.load(f)
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return render_template('index.html', posts=posts)
+
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5001, debug=True)
